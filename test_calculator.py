6637e1ce5efbf4a4428172035368e2d5647b0e43
ChristianShaw <christia.shaw@ufl.edu>
2025-04-14 19:03:21 -0400

    modifies test_calculator

--- test_calculator.py
+++ test_calculator.py
@@ -1,55 +1,89 @@
+# https://github.com/ChristianShaw980/lab10-LC-CS
+# Partner 1: Leon Calef
+# Partner 2: Christian Shaw
+
+import math
 import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self): # 3 assertions
+        a = input('Enter a number: ')
+        b = input('Enter another number: ')
+        self.assertTrue(a.isnumeric())
+        self.assertTrue(b.isnumeric())
+        a = int(a)
+        b = int(b)
+        self.assertEqual(add(a,b), (a+b))
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_subtract(self): # 3 assertions
+        a = input('Enter a number: ')
+        b = input('Enter another number: ')
+        self.assertTrue(a.isnumeric())
+        self.assertTrue(b.isnumeric())
+        a = int(a)
+        b = int(b)
+        self.assertEqual(subtract(a,b), (a-b))
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self): # 3 assertions
+        a = input('Enter a number: ')
+        b = input('Enter another number: ')
+        self.assertTrue(a.isnumeric())
+        self.assertTrue(b.isnumeric())
+        a = int(a)
+        b = int(b)
+        self.assertEqual(mul(a,b), (a*b))
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide(self): # 3 assertions
+        a = input('Enter a number: ')
+        b = input('Enter another number: ')
+        self.assertTrue(a.isnumeric())
+        self.assertTrue(b.isnumeric())
+        a = int(a)
+        b = int(b)
+        self.assertEqual(div(a,b), (a/b))
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self): # 1 assertion
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 5)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self):  # 3 assertions
+        a = input('Enter a number: ')
+        b = input('Enter another number: ')
+        self.assertTrue(a.isnumeric())
+        self.assertTrue(b.isnumeric())
+        a = int(a)
+        b = int(b)
+        self.assertEqual(logarithm(a,b), (math.log(b,a)))
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
+    def test_log_invalid_base(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            logarithm(0, 5)
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            logarithm(5, 0)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self):  # 3 assertions
+        a = input('Enter a number: ')
+        b = input('Enter another number: ')
+        self.assertTrue(a.isnumeric())
+        self.assertTrue(b.isnumeric())
+        a = int(a)
+        b = int(b)
+        self.assertEqual(hypotenuse(a,b), (math.hypot(b,a)))
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+    def test_sqrt(self):  # 3 assertions
+        a = input('Enter a number: ')
+        self.assertTrue(a.isnumeric())
+        a = int(a)
+        with self.assertRaises(ValueError):
+            square_root(a)
+        self.assertEqual(square_root(a), (math.sqrt(a)))
 
 # Do not touch this
 if __name__ == "__main__":
